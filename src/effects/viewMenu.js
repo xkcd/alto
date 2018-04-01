b026aef4f4c9adc9ab83792a62c9b8f1232888ec
zetlen <zetlen@gmail.com>
2018-04-01 17:45:17 -0500

    editmode and viewmenu

---
+++ src/effects/viewMenu.js
@@ -0,0 +1,19 @@
+/* sure, minimize the mouse cursor, that's what we meant */
+/* TODO ask folks how to upload a static asset */
+export function viewMinimize() {
+  document.body.style.cursor = "url(https://xkcd.com/1975/Tiny.cur), default";
+}
+
+/* full screen it is! */
+export function fullScreen() {
+  const comic  = document.getElementById('comic');
+  const methodName = [
+    "requestFullscreen",
+    "webkitRequestFullscreen",
+    "mozRequestFullScreen",
+    "msRequestFullscreen"
+  ].find(fname => typeof comic[fname] === "function");
+  if (methodName) {
+    comic[methodName]();
+  }
+}
