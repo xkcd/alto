3de7f411d9e244364446112e5c402735a2f2b0c3
Max Goodman <c@chromakode.com>
2018-04-01 14:04:15 -0700

    Add menu max width

--- src/ui/menu.js
+++ src/ui/menu.js
@@ -14,6 +14,7 @@ const styles = css`
   .menu {
     position: fixed;
     display: flex;
+    max-width: 250px;
     background-color: @itemBackgroundColor;
     border-radius: 3px;
     box-shadow: 0 0 10px rgba(0, 0, 0, .45);
